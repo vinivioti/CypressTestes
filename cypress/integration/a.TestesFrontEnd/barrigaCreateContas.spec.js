5913b827e49b9520f165a8bcabecc5f4fb898cf4
Vioti <82620421+vinivioti@users.noreply.github.com>
2023-08-15 22:57:22 -0300

    Update barrigaCreateContas.spec.js

--- cypress/integration/a.TestesFrontEnd/barrigaCreateContas.spec.js
+++ cypress/integration/a.TestesFrontEnd/barrigaCreateContas.spec.js
@@ -5,7 +5,7 @@ import loc from "../../support/locators"
 describe('Manipular Contas', () => {
 
     before(() => {
-      cy.login('vinivioti@yahoo.com.br', 'Cypress@123')
+      cy.login('testelino@teste.com', 'Cypress@123')
     //  cy.resetApp()
     })    
 
@@ -47,54 +47,5 @@ it('Criar Conta', () => {
                   
      })
 
-
-// =-=-=-=-=-=- PROCESSO REFATORADO USANDO LOCATORS =-=-=-=-=-=-=                  
-
-    
-/*     before(() => {
-        cy.visit('http://barrigareact.wcaquino.me/')
-     //   cy.get(loc.LOGIN.USER).type('vinivioti@yahoo.com.br')
-        cy.xpath(loc.LOGIN.XP_USER).type('vinivioti@yahoo.com.br')
-        cy.get(loc.LOGIN.PASSWORD).type('Cypress@123')
-        cy.get(loc.LOGIN.BTN_LOGIN).click()
-        cy.get(loc.MESSAGE.GET_MESSAGE).should('have.text', 'Bem vindo, Vinissius!')
-       // cy.wait(1000)
-        cy.get(loc.MESSAGE.CLOSE_MESSAGE).click
-    })    
-
-    it.only('Criar Conta', () => {
-      
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Primeira Conta Vioti')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
-        cy.wait(2000)
-        cy.get(loc.MESSAGE.GET_MESSAGE).should('have.text', 'Conta inserida com sucesso!')
-        cy.get(loc.MESSAGE.CLOSE_MESSAGE).click
-         
-                      
-     })
-
-     it('Alterar Conta', () => {
-      
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME)
-        .clear()
-        .type('Conta Vioti Alterada')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
-        cy.wait(2000)
-        cy.get(loc.MESSAGE.GET_MESSAGE).should('have.text', 'Conta atualizada com sucesso!')
-       
-                      
-     })
-
-       it('Resetar Massa no site Conta', () => {
-          cy.get('.dropdown-toggle > .fas').click() 
-          cy.get('[href="/reset"]').click()
-     
-                      
-     })
- */
-     
+  
 })
